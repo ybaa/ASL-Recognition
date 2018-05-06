946836940cfcc35d45e776e37cb2a2c3fc71eacb
MilBia <218295@student.pwr.wroc.pl>
2018-05-06 23:46:44 +0200

    basic larnign and save dataSeta

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,50 @@
-import cv2
+from skimage import data, io
+from sklearn.svm import SVC
+from sklearn.externals import joblib
 
 from src import ORB
-from src.HoG import hog_compute, histogramVisualization
-from src.ORB import pictureFromORB
+from src.BRIEFbinaryDescription import BRIEF_skimag
 
 if __name__ == '__main__':
-    image = cv2.imread('images/Machine-Learning-hero.jpg', 0)
-    keyPoints = ORB.ORB(image)
-    pictureFromORB(image)
 
-    hog = hog_compute(image)
-    histogramVisualization(hog)
-    print("lubie placki")
+    images = io.imread_collection("images/big/*.jpg")
+    learnNames = []
+    learnKeyPoints = []
+
+    for image, file in zip(images, images.files):
+        #cv2 ORB version
+        # keyPoints = ORB.ORB(image)
+        # x = []
+        # for point in keyPoints:
+        #     x.append(point.pt[0])
+        #     x.append(point.pt[1])
+        # if len(x) > 300:
+        #     learnNames.append(file[11])
+        #     learnKeyPoints.append(x)
+
+        #skimage BRIEF version
+        keyPoints = BRIEF_skimag(image)
+        if len(keyPoints)>0:
+            x = []
+            for point in keyPoints:
+                x.append(point[0])
+                x.append(point[1])
+            if len(x) > 4:
+                learnNames.append(file[11])
+                learnKeyPoints.append(x)
+
+    minSize = len(learnKeyPoints[0])
+    for points in learnKeyPoints:
+        lenght = len(points)
+        if minSize > lenght:
+            minSize = lenght
+
+    newLearnKeyPoints = []
+    for points in learnKeyPoints:
+        newLearnKeyPoints.append(points[:minSize])
+
+    clf = SVC()
+
+    clf.fit(newLearnKeyPoints, learnNames)
+    with open('BriefDataSet.pkl', "wb") as file:
+        joblib.dump(clf, file)
