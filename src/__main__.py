c73fce5610aa9833cce4464b5995ce0d1f4aca63
Martyna Łagożna <m.lagozna@live.com>
2018-05-27 16:42:20 +0200

    gaussian blutr

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,17 @@ from src.machineLearning.LearningManager import __Testing_learning_parameters__,
 if __name__ == '__main__':
 
     learning_Manager = LearningManager(testing=False, c_in=1, gamma_in='auto', decision='ovo')
-    images = ConcateHorizontalAndVertical("images/sma")
+    gaussianParams = {
+        'sigma': 1,
+        'output': None,
+        'mode': 'nearest',
+        'cval': 0,
+        'multichannel': None,
+        'preserve_range': False,
+        'truncate': 4.0
+    }
+    # if second param is true, then gaussian blur will be done
+    images = ConcateHorizontalAndVertical("images/sma", True, gaussianParams)
     trainingSet, testingSet = DevideImagesForTrainingAndTesting(images, 0.7)
 
     # __Testing_learning_parameters__(trainingSet, testingSet)
--- src/imagesOperations/ImagesCollectionLoader.py
+++ src/imagesOperations/ImagesCollectionLoader.py
@@ -1,32 +1,50 @@
-from skimage import transform, novice, color, io
+from skimage import transform, novice, color, io, filters
 import os
 
 
-def LoadImages(dirName):
+def LoadImages(dirName, doGaussianlur, gaussianParams):
     images = LoadCollectionFromDir(dirName)
-    horizontalImages, verticalImages = DevideToHorizontalAndVerticalCollections(images)
+    horizontalImages, verticalImages = DevideToHorizontalAndVerticalCollections(images, doGaussianlur, gaussianParams)
     return horizontalImages, verticalImages
 
 
 def LoadCollectionFromDir(dirName):
     return io.imread_collection(dirName + "/*.jpg")
 
-def DevideToHorizontalAndVerticalCollections(images):
+def DevideToHorizontalAndVerticalCollections(images, doGaussianBlur, gaussianParams):
     horizontalImages = []
     verticalImages = []
     for image, file in zip(images, images.files):
         if image.shape[0] > image.shape[1]:
             image = transform.resize(image, (480, 320))
+            if(doGaussianBlur):
+                filters.gaussian(image,
+                                 gaussianParams['sigma'],
+                                 gaussianParams['output'],
+                                 gaussianParams['mode'],
+                                 gaussianParams['cval'],
+                                 gaussianParams['multichannel'],
+                                 gaussianParams['preserve_range'],
+                                 gaussianParams['truncate'], )
             horizontalImages.append((image, file[11]))
         else:
             image = transform.resize(image, (320, 480))
+            if (doGaussianBlur):
+                filters.gaussian(image,
+                                 gaussianParams['sigma'],
+                                 gaussianParams['output'],
+                                 gaussianParams['mode'],
+                                 gaussianParams['cval'],
+                                 gaussianParams['multichannel'],
+                                 gaussianParams['preserve_range'],
+                                 gaussianParams['truncate'], )
             verticalImages.append((image, file[11]))
 
     return horizontalImages, verticalImages
 
 
-def ConcateHorizontalAndVertical(srcFile):
-    horizontalImages, verticalImages = LoadImages(srcFile)
+def ConcateHorizontalAndVertical(srcFile, doGaussianBlur, gaussianParams):
+    horizontalImages, verticalImages = LoadImages(srcFile, doGaussianBlur, gaussianParams)
     return horizontalImages + verticalImages
 
 
